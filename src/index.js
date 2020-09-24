529afc85a217379e56bf0c082874830a75e81a91
Paul Rogers <progers999@hotmail.co.uk>
2020-09-24 16:16:47 +1200

    Navbar markup completed and styled

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import {renderHome} from "./page_loader.js";
 
 renderHome();
+
+const renderTabs = () => {
+	const content = document.querySelector('#content');
+
+	const navContainer = document.createElement('nav');
+	navContainer.setAttribute('class', 'navbar');
+	navContainer.classList.add('pb-0');
+
+	const navbar = document.createElement('div');
+	navbar.setAttribute('class', 'navbar-nav');
+	navbar.classList.add('w-50');
+
+	const homeTab = document.createElement('a');
+	homeTab.setAttribute('class', 'nav-link');
+	homeTab.classList.add('active');
+	homeTab.textContent = 'Home';
+
+	const menuTab = document.createElement('a');
+	menuTab.setAttribute('class', 'nav-link');
+	menuTab.textContent = 'Menu';
+
+	const contactTab = document.createElement('a');
+	contactTab.setAttribute('class', 'nav-link');
+	contactTab.textContent = 'Contact';
+
+	navbar.append(homeTab, menuTab, contactTab);
+
+	navContainer.append(navbar);
+
+	content.insertAdjacentElement('beforebegin', navContainer);
+	return {homeTab, menuTab, contactTab};
+}
+
+renderTabs();
